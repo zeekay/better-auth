830b57213c2b55daae8a2c2abc6ae1c05a25e63e
Shawn Erquhart <shawn@erquh.art>
2025-08-10 13:54:07 -0400

    docs: add anonymous login to nextjs example

--- examples/next/lib/auth-client.ts
+++ examples/next/lib/auth-client.ts
@@ -3,12 +3,14 @@ import {
   magicLinkClient,
   emailOTPClient,
   genericOAuthClient,
+  anonymousClient,
 } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 import { convexClient } from "@convex-dev/better-auth/client/plugins";
 
 export const authClient = createAuthClient({
   plugins: [
+    anonymousClient(),
     magicLinkClient(),
     emailOTPClient(),
     twoFactorClient(),
