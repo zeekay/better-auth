6ca46a871987433f3b583bf97bff01a5d1af2654
Shawn Erquhart <shawn@erquh.art>
2025-12-07 22:35:02 -0500

    wip

--- examples/react/convex/http.ts
+++ examples/react/convex/http.ts
@@ -1,8 +1,14 @@
 import { httpRouter } from "convex/server";
 import { authComponent, createAuth } from "./auth";
 
+console.log("test");
+
 const http = httpRouter();
 
+console.log("test2");
+
 authComponent.registerRoutes(http, createAuth, { cors: true });
+
+console.log("test3");
 
 export default http;
