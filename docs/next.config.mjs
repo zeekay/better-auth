651ad3e79a5eb85b2e1653a34868f31df43933d6
Shawn Erquhart <shawn@erquh.art>
2025-12-16 17:10:31 -0500

    update deps

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -5,6 +5,25 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  redirects: async () => {
+    return [
+      {
+        source: "/local-install",
+        destination: "/features/local-install",
+        permanent: true,
+      },
+      {
+        source: "/authorization",
+        destination: "/basic-usage/authorization",
+        permanent: true,
+      },
+      {
+        source: "/triggers",
+        destination: "/features/triggers",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default withMDX(config);
